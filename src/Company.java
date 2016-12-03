98de72fbba15340e7c95cad6a685331197d31cd7
Haaris Memon <haaris.memon@kcl.ac.uk>
2016-12-03 12:47:35 +0000

    Add a class to represent a Company

---
+++ src/Company.java
@@ -0,0 +1,12 @@
+/**
+ * This class represents a Company that the user has applied to.
+ */
+public class Company {
+
+    private String name;
+
+    Company(String name) {
+        this.name = name;
+    }
+
+}
