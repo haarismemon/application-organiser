0a4bc9201a12c5736f462c15e7c8004dbfe73651
Haaris Memon <haaris.memon@kcl.ac.uk>
2016-12-03 14:01:51 +0000

    Allow a Stage to have an optional completion and reply date

--- src/Stage.java
+++ src/Stage.java
@@ -1,3 +1,7 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * This class represents a particular Stage in the application process of an Internship.
  */
@@ -6,34 +10,61 @@ public class Stage {
     private String stageName;
     private boolean isCompleted;
     private boolean isWaitingForResponse;
-    private boolean isSuccessful;
+    private Boolean isSuccessful;
+    private Date dateOfCompletion;
+    private Date dateOfReply;
 
     public Stage(String stageName) {
         this.stageName = stageName;
+    }
+
+    private Date stringToDate(String dateString) {
+        Date date = null;
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try{
+            date = dateFormat.parse(dateString);
+        } catch(ParseException e) {
+            System.out.println("The date supplied is incorrect, date is set to null");
+        }
+
+        return date;
     }
 
     public boolean isCompleted() {
         return isCompleted;
     }
 
-    public boolean isWaitingForResponse() {
-        return isWaitingForResponse;
-    }
-
-    public boolean isSuccessful() {
-        return isSuccessful;
-    }
-
     public void setCompleted(boolean completed) {
         isCompleted = completed;
+    }
+
+    public void setCompleted(boolean completed, String date) {
+        setCompleted(completed);
+        this.dateOfCompletion = stringToDate(date);
+    }
+
+    public boolean isWaitingForResponse() {
+        return isWaitingForResponse;
     }
 
     public void setWaitingForResponse(boolean waitingForResponse) {
         isWaitingForResponse = waitingForResponse;
     }
 
+    public Boolean isSuccessful() {
+        return isSuccessful;
+    }
+
     public void setSuccessful(boolean successful) {
         isSuccessful = successful;
+        //if the user specifies if stage is successful or not, then they are no longer waiting for a response
+        isWaitingForResponse = false;
+    }
+
+    public void setSuccessful(boolean successful, String date) {
+        setSuccessful(successful);
+        this.dateOfReply = stringToDate(date);
     }
 
     @Override
