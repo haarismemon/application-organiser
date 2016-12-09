f8aa885e83249dca127a24315cb2fe3225e4c296
Haaris Memon <haaris.memon@kcl.ac.uk>
2016-12-09 14:37:38 +0000

    Display only relevant information in Stage View

--- src/main/java/view/StageView.java
+++ src/main/java/view/StageView.java
@@ -48,71 +48,88 @@ public class StageView extends ScrollPane {
 
         Font stageFont = new Font("Regular", 18);
 
+        int row = 0;
         Label stageTitle = new Label("Stage Name: ");
-        gp.add(stageTitle, 0, 0);
-        Label completedTitle = new Label("Completed?: ");
-        gp.add(completedTitle, 0, 1);
-        Label waitingTitle = new Label("Waiting for Response?: ");
-        gp.add(waitingTitle, 0, 2);
-        Label successfulTitle = new Label("Successful?: ");
-        gp.add(successfulTitle, 0, 3);
-        Label startDateTitle = new Label("Date of start: ");
-        gp.add(startDateTitle, 0, 4);
-        Label completionDateTitle = new Label("Date of completion: ");
-        gp.add(completionDateTitle, 0, 5);
-        Label replyDateTitle = new Label("Date of reply: ");
-        gp.add(replyDateTitle, 0, 6);
-
+        gp.add(stageTitle, 0, row);
         Label stageLabel = new Label(stage.getStageName());
-        gp.add(stageLabel, 1, 0);
+        gp.add(stageLabel, 1, row);
+
+        ++row; //row = 1
+        Label completedTitle = new Label("Completed?: ");
+        gp.add(completedTitle, 0, row);
         Label completedLabel;
         if(stage.isCompleted()) completedLabel = new Label("Yes");
         else completedLabel = new Label("No");
-        gp.add(completedLabel, 1, 1);
+        gp.add(completedLabel, 1, row);
 
-        Label waitingLabel;
-        if(stage.isWaitingForResponse()) waitingLabel = new Label("Yes");
-        else waitingLabel = new Label("No");
-        gp.add(waitingLabel, 1, 2);
+        if((stage.isCompleted() && stage.isSuccessful() == null)) {
+            ++row; //row = 2, if successful is null
+            Label waitingLabel;
+            if (stage.isWaitingForResponse()) waitingLabel = new Label("Yes");
+            else waitingLabel = new Label("No");
+            waitingLabel.setFont(stageFont);
+            gp.add(waitingLabel, 1, row);
 
-        Label successfulLabel;
-        if(stage.isSuccessful() == null) successfulLabel = new Label("-");
-        else if (stage.isSuccessful()) successfulLabel = new Label("Yes");
-        else successfulLabel = new Label("No");
-        gp.add(successfulLabel, 1, 3);
+            Label waitingTitle = new Label("Waiting for Response?: ");
+            gp.add(waitingTitle, 0, row);
+            waitingTitle.setFont(stageFont);
+        }
 
+        if(stage.isCompleted() && !stage.isWaitingForResponse()) {
+            ++row; // row = 3, if successful is null and waiting is false
+            Label successfulLabel;
+            if (stage.isSuccessful() == null) successfulLabel = new Label("-");
+            else if (stage.isSuccessful()) successfulLabel = new Label("Yes");
+            else successfulLabel = new Label("No");
+            successfulLabel.setFont(stageFont);
+            gp.add(successfulLabel, 1, row);
+
+            Label successfulTitle = new Label("Successful?: ");
+            gp.add(successfulTitle, 0, row);
+            successfulTitle.setFont(stageFont);
+        }
+
+        ++row; //row = 4
+        Label startDateTitle = new Label("Date of start: ");
+        gp.add(startDateTitle, 0, row);
         Label startDateLabel;
         if(stage.getDateOfStart() == null) startDateLabel = new Label("-");
         else startDateLabel = new Label(ParseApplications.formatDate(stage.getDateOfStart()));
-        gp.add(startDateLabel, 1, 4);
+        gp.add(startDateLabel, 1, row);
 
-        Label completionDateLabel;
-        if(stage.getDateOfCompletion() == null) completionDateLabel = new Label("-");
-        else completionDateLabel = new Label(ParseApplications.formatDate(stage.getDateOfCompletion()));
-        gp.add(completionDateLabel, 1, 5);
+        if(stage.isCompleted()) {
+            ++row; //row = 5
+            Label completionDateTitle = new Label("Date of completion: ");
+            gp.add(completionDateTitle, 0, row);
+            Label completionDateLabel;
+            if (stage.getDateOfCompletion() == null) completionDateLabel = new Label("-");
+            else completionDateLabel = new Label(ParseApplications.formatDate(stage.getDateOfCompletion()));
+            gp.add(completionDateLabel, 1, row);
+            completionDateTitle.setFont(stageFont);
+            completionDateLabel.setFont(stageFont);
+        }
 
-        Label replyDateLabel;
-        if(stage.getDateOfReply() == null) replyDateLabel = new Label("-");
-        else replyDateLabel = new Label(ParseApplications.formatDate(stage.getDateOfReply()));
-        gp.add(replyDateLabel, 1, 6);
+        if(stage.isCompleted() && !stage.isWaitingForResponse()) {
+            ++row; //row = 6
+            Label replyDateTitle = new Label("Date of reply: ");
+            gp.add(replyDateTitle, 0, row);
+            Label replyDateLabel;
+            if (stage.getDateOfReply() == null) replyDateLabel = new Label("-");
+            else replyDateLabel = new Label(ParseApplications.formatDate(stage.getDateOfReply()));
+            gp.add(replyDateLabel, 1, row);
+            replyDateTitle.setFont(stageFont);
+            replyDateLabel.setFont(stageFont);
+        }
+
 
         stageTitle.setFont(stageFont);
         completedTitle.setFont(stageFont);
-        waitingTitle.setFont(stageFont);
-        successfulTitle.setFont(stageFont);
         startDateTitle.setFont(stageFont);
-        completionDateTitle.setFont(stageFont);
-        replyDateTitle.setFont(stageFont);
         stageLabel.setFont(stageFont);
         completedLabel.setFont(stageFont);
-        waitingLabel.setFont(stageFont);
-        successfulLabel.setFont(stageFont);
         startDateLabel.setFont(stageFont);
-        completionDateLabel.setFont(stageFont);
-        replyDateLabel.setFont(stageFont);
-        
-        vBox.getChildren().add(gp);
 
+        vBox.getChildren().add(gp);
 
         root.setCenter(vBox);
 
